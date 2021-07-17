7ceaeddd00b269996079243bcfb6691d1825e777
xxooxxooxx <panchunmei30@gmail.com>
2021-07-18 00:18:58 +0800

    Create sh.sh

---
+++ sh.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source /etc/profile
+wget http://ftp.apnic.net/apnic/stats/apnic/delegated-apnic-latest
+echo "/ip firewall address-list" > ip_address.rsc
+grep "|CN|ipv4" delegated-apnic-latest | awk -F'|' '{print "add address="$4"/"32-int(log(int($5))/log(2))" disabled=no list=china-ip"}' >> ip_address.rsc
+rm delegated-apnic-latest
